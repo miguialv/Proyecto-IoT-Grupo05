b05da267d98da24e360ecb88c30db3b05a0d7520
Vicent Cosme <vicentcosme6@gmail.com>
2019-10-22 18:28:20 +0200

    Proyecto androidthings
    solo he creado el proyecto para el firebase

---
+++ AndroidThingsE7/app/src/main/java/com/nearby/androidthingse7/MainActivity.java
@@ -0,0 +1,21 @@
+package com.nearby.androidthingse7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+}
